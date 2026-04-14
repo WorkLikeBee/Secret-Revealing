583657300341468877fb3e39a5fa15ebfc0d3ddf
Dat Ngo <datngo@Dats-MacBook-Pro.local>
2026-04-13 22:48:54 -0700

    Make port dynamic

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 // 2. Create an express app and set the port number.
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // 3. Use the public folder for static files.
 app.use(express.static("public"));
